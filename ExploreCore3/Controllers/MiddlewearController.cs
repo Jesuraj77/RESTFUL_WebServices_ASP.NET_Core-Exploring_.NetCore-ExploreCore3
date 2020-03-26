e4651134e5a4f532d786f55e8b7f5452b0969c55
itdev-lenovo <it1devops@gmail.com>
2020-03-26 20:32:02 +0530

    files added

---
+++ ExploreCore3/Controllers/MiddlewearController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreCore3.Controllers
+{
+    public class MiddlewearController : Controller
+    {
+      //  Create a custom middleware to log the details of the incoming request.
+  
+
+        //Integrate the Middleware in the startup class pipeline.
+
+
+        //Configure custom values like FirstName and LastName in a Configuration JSON File and inject the same in the Custom Middleware pipeline.
+
+    }
+}
